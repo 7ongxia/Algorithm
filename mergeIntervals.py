0c99bc1c4002fa174bc0d0fff5123fa5ce0baa6a
7ongxia <7ongxia@gmail.com>
2021-12-12 21:28:49 +0800

    56. Merge Intervals
    https://leetcode.com/problems/merge-intervals/

---
+++ mergeIntervals.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda l:l[0])
+        ans = []
+        before = intervals[0]
+        
+        for interval in intervals:
+            if interval[0] <= before[1] and interval[1] >= before[1]:
+                if interval[1] >= before[1]:
+                    before[1] = interval[1]
+                if interval[0] <= before[0]:
+                    before[0] = interval[0]
+            elif before[0] <= interval[0] and before[1] >= interval[1]:
+                continue
+            else:
+                ans.append(before)
+                before = interval
+        
+        ans.append(before)
+        
+        return ans
+            
